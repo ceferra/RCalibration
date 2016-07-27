4c7151adb351a2933a24098d794460b2e5283a5d
ceferra <ceferra@gmail.com>
2016-07-27 15:56:42 +0200

    First commit

--- calibrationMethods.R
+++ calibrationMethods.R
@@ -1,3 +1,11 @@
+
+### Code for the paper
+##
+#Applied Intelligence
+#June 2013, Volume 38, Issue 4, pp 566–585
+#On the effect of calibration in classifier combination
+#Antonio Bella, Cèsar Ferri, José Hernández-Orallo, María José Ramírez-Quintana
+
 # Function to assign the calibrated probability from the "$interval" and "$calProb"
 # INPUTS
 # ------
